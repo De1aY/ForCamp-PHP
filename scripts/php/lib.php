70f5730cd4e115d3a2ef9ab410fc66a4fa93756d
De1aY <6de1ay@gmail.com>
2016-12-23 12:49:20 +0300

    Added: 3 new classes

--- scripts/php/lib.php
+++ scripts/php/lib.php
@@ -1,4 +1,15 @@
 <?php
+
+    /*
+    Коды:
+    200 - OK
+    500 - Ошибка при создании подключения к базе данных
+    501 - Ошибка при выполнении запроса к базе данных
+    600 - Неверный формат входных данных
+    601 - Неверный логин/пароль
+    602 - Неверный токен
+    */
+
 	define("MYSQL_SERVER", "52.169.122.82");  // IP сервера MySQL
 	define("MYSQL_LOGIN", "root");  // Логин сервера MySQL
 	define("MYSQL_PASSWORD", "5zaU2x8A");  // Пароль сервера MySQL
@@ -14,74 +25,107 @@
         echo json_encode($Array);
     }
 
-    class DataBase_Connection{  // Класс для работы с базой данных
-        function BaseInit(){  // Создание подключения к базе данных со стандартными параметрами
+    class DataBase{  // Класс для работы с базой данных
+        protected var $DataBase_Connection;
+
+        protected function BaseInit(){  // Создание подключения к базе данных со стандартными параметрами
             try{
                 $Connection = new mysqli(MYSQL_SERVER, MYSQL_LOGIN, MYSQL_PASSWORD, MYSQL_DB);
-                return $Connection;  // 200 - OK
+                return $Connection;
             }catch(Exception $e){
                 return 500;  // 500 - Ошибка при создании подключения к базе данных
             }
         }
 
-        function AdvancedInit($MySQL_Server, $MySQL_Login, $MySQL_Password, $MySQL_DB){  // Создания подключения к базе данных с заданными параметрами
+        protected function AdvancedInit($MySQL_Server, $MySQL_Login, $MySQL_Password, $MySQL_DB){  // Создания подключения к базе данных с заданными параметрами
             try{
                 $Connection = new mysqli($MySQL_Server, $MySQL_Login, $MySQL_Password, $MySQL_DB);
-                return $Connection;  // 200 - OK
+                return $Connection;
             }catch(Exception $e){
                 return 500;  // 500 - Ошибка при создании подключения к базе данных
             }
         }
     }
 
-    class Authorization{
-        var $User_Login;
-        var $User_Password;
-        var $Token = NULL;
-        var $Platform;
+    class Authorization_Core extends DataBase{
+        protected var $User_Login = NULL;
+        protected var $User_Password = NULL;
+        protected var $User_Platform = NULL;
+        protected var $User_Token = NULL;
+        protected var $Status = 200;
 
-        function Authorization($Login, $Password, $Platform, $Token = NULL){
-            if(isset($Login) and isset($Password) and isset($Platform)){
-                $this->$User_Login = $Login;
-                $this->$User_Password = $Password;
-                $this->$Platform = $Platform."Token";
+        protected function Authorize(){
+            if(isset($this->User_Token)){
+                $this->Authorize_WithToken();
+            }
+            else{
+                $this->Authorize_WithoutToken();
+            }
+        }
+
+        private function Authorize_WithToken(){
+            
+        }
+
+        private function Authorize_WithoutToken(){
+
+        }
+
+        protected function PrintError($ErrorCode, $ErrorStage){
+            error_log($ErrorStage." error ".$ErrorCode);
+            $Array = array("status" => "ERROR", "token" => "", "code" => $ErrorCode);
+            EchoJSON($Array);
+            $this->Status = $ErrorCode;
+            return $ErrorCode;
+        }
+
+        protected function SetDataBaseConnection(){
+            $this->DataBase_Connection = $this->BaseInit();
+            if($this->DataBase_Connection == 500){
+                return PrintError(500, "Authorize->SetDataBaseConnection");  // 500 - Ошибка при создании подключения к базе данных
+            }
+        }
+    }
+
+    class Authorization_Web extends Authorization_Core{
+
+        function Authorization_Web($Login = NULL, $Password = NULL, $Token = NULL){
+            $this->User_Platform = "WEBToken";
+            if(isset($Login) and isset($Password)){
+                $this->User_Login = $Login;
+                $this->User_Password = $Password;
+                $this->SetDataBaseConnection();
+            }
+            else{
                 if(isset($Token)){
-                    $this->$Token = $Token;
-                    return 200;  // 200 - OK
+                    $this->User_Token = $Token;
+                    $this->SetDataBaseConnection();
                 }
-                return 200;  //200 - OK
-            }
-            else{
-                $Array = array("status" => "ERROR", "token" => "", "code" => 600);
-                EchoJSON($Array);
-                return 600;  // 600 - Строка пуста
-            }
-        }
-    }
-
-    class WebAuthorization extends DataBase_Connection{
-
-        function WebAuthorization(){
-            $this->$DataBase = $this->BaseInit();
-            if($this->$DataBase != 500){
-            
-            }
-            else{
-                return $this->$DataBase;
+                else{
+                    return PrintError(600, "Authorization_Web");  // 600 - Неверный формат входных данных
+                }
             }
         }
 
     }
 
-    class MobileAuthorization extends DataBase_Connection{
-        
-        function MobileAuthorization(){
-            $this->$DataBase = $this->BaseInit();
-            if($this->$DataBase != 500){
-            
+    class Authorization_Mobile extends Authorization_Core{
+
+        function Authorization_Mobile($Login = NULL, $Password = NULL, $Token = NULL){
+            $this->User_Platform = "WEBToken";
+            if(isset($Login) and isset($Password)){
+                $this->User_Login = $Login;
+                $this->User_Password = $Password;
+                $this->SetDataBaseConnection();
             }
             else{
-                return $this->$DataBase;
+                if(isset($Token)){
+                    $this->User_Token = $Token;
+                    $this->SetDataBaseConnection();
+                }
+                else{
+                    return PrintError(600, "Authorization_Web");  // 600 - Неверный формат входных данных
+                }
             }
         }
     }
